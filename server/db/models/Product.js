2144374a3e0b3e72cad34d92a4ba0c8cf4ed9c1a
kohlbrr <kohlbrr@gmail.com>
2017-06-05 16:50:05 -0400

    Added Product model and some basic mocha/chai tests

---
+++ server/db/models/Product.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../index');
+
+const Product = db.define('product', {
+  size: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  stock: {
+    type: Sequelize.INTEGER
+  },
+  imageUrl: {
+    type: Sequelize.STRING
+  }
+});
+
+module.exports = Product;
---
+++ server/specs/models/product.spec.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const expect = require('chai').expect;
+const { Product } = require('../../db/models');
+
+describe('Product Model', () => {
+  beforeEach(() => {
+    return Product.sync({force: true});
+  });
+
+  describe('Validators', () => {
+    describe('Size', () => {
+
+      it('Should throw an error if not a string', () => {
+        let product = Product.build({
+          color: 'Blue',
+          size: []
+        });
+        return product.validate().then(err => {
+          expect(err).to.exist;
+        });
+      });
+
+      it('Should throw an error if not defined', () => {
+        let product = Product.build({
+          color: 'Red'
+        });
+        return product.validate().then(err => {
+          expect(err).to.exist;
+        });
+      });
+    });
+
+    describe('Color', () => {
+
+      it('Should throw an error if not a string', () => {
+        let product = Product.build({
+          color: [],
+          size: 'Small'
+        });
+        return product.validate().then(err => {
+          expect(err).to.exist;
+        });
+      });
+
+      it('Should throw an error if not defined', () => {
+        let product = Product.build({
+          size: 'Large'
+        });
+        return product.validate().then(err => {
+          expect(err).to.exist;
+        });
+      });
+    });
+  });
+});
