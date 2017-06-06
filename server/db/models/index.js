1d80249b6a4e4ec60bde3d30bad7c2796d9dbe3b
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-06 12:34:38 -0400

    finished model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,8 +1,10 @@
 const Product = require('./Product');
 const User = require('./User');
+const Review = require('./Review');
 // Product.belongsTo()\
 
 module.exports = {
   Product,
   User,
+  Review,
 };
