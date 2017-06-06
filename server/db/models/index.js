1230fc2d69f94009f0df9823efd61db47548e460
kohlbrr <kohlbrr@gmail.com>
2017-06-06 12:26:15 -0400

    Design model and basic test spec created

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,9 @@
 const Product = require('./Product');
+const Design = require('./Design');
 
 // Product.belongsTo()\
 
 module.exports = {
-  Product
+  Product,
+  Design
 };
--- server/specs/models/design.spec.js
+++ server/specs/models/design.spec.js
@@ -24,7 +24,8 @@ describe('Design Model', () => {
 
       it('Should throw an error if not defined', () => {
         let design = Design.build({
-          color: 'Red'
+          sex: 'F',
+          price: 1500
         });
         return design.validate().then(err => {
           expect(err).to.exist;
@@ -32,25 +33,48 @@ describe('Design Model', () => {
       });
     });
 
-    describe('Color', () => {
+    describe('Sex', () => {
 
-      it('Should throw an error if not a string', () => {
+      it('Should throw an error if not one of the enum values', (done) => {
+        let design = Design.create({
+          sex: 'A',
+          name: 'Salt in the wind esq.',
+          price: 8000000
+        })
+        .then(() => (done('I shoud not get here!')))
+        .catch(() => done());
+      });
+
+      it('Should throw an error if not defined', () => {
         let design = Design.build({
-          color: [],
-          name: 'Small'
+          name: 'Plain White Shirt',
+          price: 100000000
         });
         return design.validate().then(err => {
           expect(err).to.exist;
         });
       });
+    });
 
-      it('Should throw an error if not defined', () => {
-        let design = Design.build({
-          name: 'Large'
-        });
-        return design.validate().then(err => {
-          expect(err).to.exist;
-        });
+    describe('Price', () => {
+    
+      it('Should throw an error if not an integer', (done) => {
+        let design = Design.create({
+          name: 'I <3 NY',
+          sex: 'M',
+          price: 22.95
+        })
+        .then(() => done('I should not get here!'))
+        .catch(() => done());
+      });
+      
+      it('Should throw an error if not defined', (done) => {
+        let design = Design.create({
+          name: 'I HATE NY',
+          sex: 'F',
+        })
+        .then(() => done('I should not get here!'))
+        .catch(() => done());
       });
     });
   });
