b4476966305f8610fc2ffc8a4d0811f4a177fa95
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-06 14:33:01 -0400

    erros on associations·

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,7 +3,35 @@ const User = require('./User');
 const Cartcontents = require('./Cartcontents');
 const Review = require('./Review');
 const Design = require('./Design');
-// Product.belongsTo()\
+const Category = require('./Category');
+const Clothing = require('./Clothing');
+const Order = require('./Order');
+const OrderProducts = require('./OrderProducts');
+
+// Product.belongsTo(Design);
+// Product.belongsToMany(Order, { through: OrderProducts });
+// Product.belongsToMany(User, { through: Cartcontents });
+
+// Design.hasMany(Product);
+// Design.belongsTo(Clothing);
+// Design.belongsTo(Category);
+// Design.hasMany(Review);
+
+// Clothing.hasMany(Category);
+// Clothing.hasMany(Design);
+
+// Category.belongsTo(Clothing);
+// Category.hasMany(Design);
+
+// User.hasMany(Review);
+// User.belongsToMany(Product, { through: Cartcontents });
+// User.hasMany(Order);
+
+// Review.belongsTo(User);
+// Review.belongsTo(Design);
+
+// Order.belongsTo(User);
+// Order.belongsToMany(Product, { through: OrderProducts });
 
 module.exports = {
   Product,
@@ -11,4 +39,8 @@ module.exports = {
   Cartcontents,
   Review,
   Design,
+  Category,
+  Clothing,
+  Order,
+  OrderProducts,
 };
