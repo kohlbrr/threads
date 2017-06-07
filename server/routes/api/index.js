053e9aa5b1922fb5d5c92166400c20988de74adb
Joon Kim <jkim7078@gmail.com>
2017-06-07 13:52:33 -0400

    made product api routes that hasn't been tested

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const designRoutes = require('./designs');
+const productRoutes = require('./products');
 
 router.use('/designs', designRoutes);
+
+router.use('/products', productRoutes);
 
 module.exports = router;
