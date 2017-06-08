378138b9682a231c1955afb1835e2b1729f2fdfd
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-08 15:47:49 -0400

    clothings routes passing tests

--- server/specs/routes/clothings.spec.js
+++ server/specs/routes/clothings.spec.js
@@ -1,13 +1,13 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../app');
-const { Category, Design, User } = require('../../db/models');
+const { Clothing, Category, User } = require('../../db/models');
 
 const agent = session(app);
 
-describe('Category API routes', () => {
-  beforeEach(() => Category.sync({ force: true })
-    .then(() => Design.sync({ force: true }))
+describe('Clothing API routes', () => {
+  beforeEach(() => Clothing.sync({ force: true })
+    .then(() => Category.sync({ force: true }))
     .then(() => User.sync({ force: true })));
   beforeEach(() => User.create(
     {
@@ -20,59 +20,55 @@ describe('Category API routes', () => {
       isAdmin: false,
     })));
 
-  describe('GET /api/categories', () => {
-    beforeEach(() => Category.create({
+  describe('GET /api/clothings', () => {
+    beforeEach(() => Clothing.create({
       name: 'T-Shirt',
     })
-    .then(category => Design.create({
+    .then(clothing => Category.create({
       name: 'Polo',
-      price: 15,
-      sex: 'M',
-      categoryId: category.id,
+      clothingId: clothing.id,
     })));
-    it('responds with 200', () => agent.get('/api/categories').expect(200));
-    it('responds with an Array of Categories', () =>
-        agent.get('/api/categories').expect((res) => {
+    it('responds with 200', () => agent.get('/api/clothings').expect(200));
+    it('responds with an Array of clothings', () =>
+        agent.get('/api/clothings').expect((res) => {
           expect(res.body).to.be.an('array');
           expect(res.body[0].name).to.be.equal('T-Shirt');
         }));
   });
-  describe('GET /api/categories/:id', () => {
-    beforeEach(() => Category.create({
+  describe('GET /api/clothings/:id', () => {
+    beforeEach(() => Clothing.create({
       name: 'T-Shirt',
     })
-    .then(category => Design.create({
+    .then(clothing => Category.create({
       name: 'Polo',
-      price: 15,
-      sex: 'M',
-      categoryId: category.id,
+      clothingId: clothing.id,
     })));
-    it('responds with 200', () => agent.get('/api/categories/1').expect(200));
-    it('responds with a 404 if category does not exist', () =>
-       agent.get('/api/categories/5').expect(404));
+    it('responds with 200', () => agent.get('/api/clothings/1').expect(200));
+    it('responds with a 404 if clothing does not exist', () =>
+       agent.get('/api/clothings/5').expect(404));
     it('responds with a 404 if not a valid id', () =>
-       agent.get('/api/categories/lsks').expect(404));
-    it('responds with a specific category', () =>
-       agent.get('/api/categories/1').expect(res =>
+       agent.get('/api/clothings/lsks').expect(404));
+    it('responds with a specific clothing', () =>
+       agent.get('/api/clothings/1').expect(res =>
         expect(res.body.name).to.equal('T-Shirt')));
-    it('the category has a designs property with an array of designs', () =>
-      agent.get('/api/categories/1').expect((res) => {
-        expect(res.body.designs).to.be.an('array');
-        expect(res.body.designs[0].name).to.equal('Polo');
+    it('the category has a designs property with an array of categories', () =>
+      agent.get('/api/clothings/1').expect((res) => {
+        expect(res.body.categories).to.be.an('array');
+        expect(res.body.categories[0].name).to.equal('Polo');
       }));
   });
-  describe('POST /api/categories', () => {
-    const category = {
+  describe('POST /api/clothings', () => {
+    const clothing = {
       name: 'T-Shirt',
     };
     it('should throw a 401 error if it is not login', () =>
-       agent.post('/api/categories').send(category).expect(401));
+       agent.post('/api/clothings').send(clothing).expect(401));
     it('respond with a 401 if it is login but is not an Admin', () => {
       const notAdmin = session(app);
       return notAdmin.post('/auth/login')
       .send({ email: 'notadmin@admin.com', password: 'pass123' })
       .then(() =>
-        notAdmin.post('/api/categories').send(category).expect(401));
+        notAdmin.post('/api/clothings').send(clothing).expect(401));
     });
     describe('Admin User', () => {
       let adminUser;
@@ -82,18 +78,18 @@ describe('Category API routes', () => {
         .post('/auth/login')
         .send({ email: 'admin@admin.com', password: 'pass123' });
       });
-      it('should respond with a 400 if not a valid category', () =>
-        adminUser.post('/api/categories').send({ cake: 'chocolate' }).expect(400));
+      it('should respond with a 400 if not a valid clothing', () =>
+        adminUser.post('/api/clothings').send({ cake: 'chocolate' }).expect(400));
       it('should respond with a 201', () =>
         adminUser.post('/auth/login')
         .send({ email: 'admin@admin.com', password: 'pass123' })
         .then(() =>
-          adminUser.post('/api/categories').send(category).expect(201)));
+          adminUser.post('/api/clothings').send(clothing).expect(201)));
       it('should respond with the created product', () =>
         adminUser.post('/auth/login')
         .send({ email: 'admin@admin.com', password: 'pass123' })
         .then(() =>
-          adminUser.post('/api/categories').send(category)
+          adminUser.post('/api/clothings').send(clothing)
           .expect((res) => {
             expect(res.body).to.be.an('object');
             expect(res.body.name).to.equal('T-Shirt');
@@ -101,27 +97,27 @@ describe('Category API routes', () => {
           })));
     });
   });
-  describe('PUT /api/categories/:id', () => {
-    const category = {
+  describe('PUT /api/clothings/:id', () => {
+    const clothing = {
       name: 'Pants',
     };
     let route;
     let id;
-    beforeEach(() => Category.create({
+    beforeEach(() => Clothing.create({
       name: 'T-Shirt',
     })
     .then((newDesign) => {
       id = newDesign.id;
-      route = `/api/categories/${id}`;
+      route = `/api/clothings/${id}`;
     }));
     it('should throw a 401 error if it is not login', () =>
-       agent.put(route).send(category).expect(401));
+       agent.put(route).send(clothing).expect(401));
     it('respond with a 401 if it is login but is not an Admin', () => {
       const notAdmin = session(app);
       return notAdmin.post('/auth/login')
       .send({ email: 'notadmin@admin.com', password: 'pass123' })
       .then(() => {
-        notAdmin.put(route).send(category).expect(401);
+        notAdmin.put(route).send(clothing).expect(401);
       });
     });
     describe('Admin User', () => {
@@ -131,15 +127,15 @@ describe('Category API routes', () => {
         return adminUser.post('/auth/login')
         .send({ email: 'admin@admin.com', password: 'pass123' });
       });
-      it('responds with a 404 if category does not exist', () =>
-          adminUser.put('/api/categories/5').send(category).expect(404));
+      it('responds with a 404 if clothing does not exist', () =>
+          adminUser.put('/api/clothings/5').send(clothing).expect(404));
       it('responds with a 404 if not a valid id', () =>
-          adminUser.put('/api/categories/dsk').send(category).expect(404));
+          adminUser.put('/api/clothings/dsk').send(clothing).expect(404));
       it('should respond with a 201', () =>
-          adminUser.put(route).send(category)
+          adminUser.put(route).send(clothing)
           .expect(201));
-      it('should respond with the updated category', () =>
-          adminUser.put(route).send(category)
+      it('should respond with the updated clothing', () =>
+          adminUser.put(route).send(clothing)
           .expect((res) => {
             expect(res.body).to.be.an('object');
             expect(res.body.name).to.equal('Pants');
@@ -147,21 +143,19 @@ describe('Category API routes', () => {
           }));
     });
   });
-  describe('DELETE /api/categories/:id', () => {
+  describe('DELETE /api/clothings/:id', () => {
     let route;
     let id;
-    beforeEach(() => Category.create({
+    beforeEach(() => Clothing.create({
       name: 'T-Shirt',
     })
-    .then((newCategory) => {
-      id = newCategory.id;
-      route = `/api/categories/${id}`;
+    .then((newclothing) => {
+      id = newclothing.id;
+      route = `/api/clothings/${id}`;
     })
-    .then(() => Design.create({
+    .then(() => Category.create({
       name: 'Polo',
-      price: 15,
-      sex: 'M',
-      categoryId: id,
+      clothingId: id,
     })));
     it('should throw a 401 error if it is not login', () =>
        agent.delete(route).expect(401));
@@ -180,17 +174,17 @@ describe('Category API routes', () => {
         return adminUser.post('/auth/login')
         .send({ email: 'admin@admin.com', password: 'pass123' });
       });
-      it('responds with a 404 if category does not exist', () =>
-        adminUser.delete('/api/categories/5').expect(404));
+      it('responds with a 404 if clothing does not exist', () =>
+        adminUser.delete('/api/clothings/5').expect(404));
       it('responds with a 404 if not a valid id', () =>
-        adminUser.delete('/api/categories/dsk').expect(404));
+        adminUser.delete('/api/clothings/dsk').expect(404));
       it('should respond with a 204', () =>
         adminUser.delete(route).expect(204));
-      it('should delete also all the designs associated to the category', () =>
+      it('should delete also all the designs associated to the clothing', () =>
         adminUser.delete(route)
         .then(() =>
-          Design.findAll({ where: { categoryId: id } })
-          .then(designs => expect(designs).to.have.lengthOf(0))));
+          Category.findAll({ where: { clothingId: id } })
+          .then(categories => expect(categories).to.have.lengthOf(0))));
     });
   });
 });
