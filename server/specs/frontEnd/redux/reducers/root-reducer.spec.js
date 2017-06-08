f0d56a1fffae74d4be45f175c138819a3468aa4c
kohlbrr <kohlbrr@gmail.com>
2017-06-08 18:52:38 -0400

    test(rootReducer, 77): Added initial state checks

--- server/specs/frontEnd/redux/reducers/root-reducer.spec.js
+++ server/specs/frontEnd/redux/reducers/root-reducer.spec.js
@@ -1,13 +1,26 @@
 import { expect } from 'chai';
 
 import { createStore } from 'redux';
-import mainReducer from '../../../../../browser/src/react/reducers/root-reducer';
+import rootReducer from '../../../../../browser/src/react/reducers/root-reducer';
 
 describe('Root reducer', () => {
   
   let testStore;
   beforeEach('Create a test store', () => {
-    
+    testStore = createStore(rootReducer);
+  });
+
+  it('has expected initial state', () => {
+    expect(testStore.getState()).to.be.deep.equal({
+      currentUser: {},
+      cart: [{}],
+      orders: [{}],
+      currentOrder: {},
+      currentProduct: {},
+      currentDesign = {}, // contains reviews and products
+      categories: [{}],
+      currentCategory: {} // contains designs
+    });
   });
 
 });
