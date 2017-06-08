715ae5425dd4a425d161183ddafc39019d68be4e
Hubert J <hubejan@gmail.com>
2017-06-08 12:32:30 -0400

    Base skeleton for frontend. Including set-up for redux.

---
+++ browser/src/react/routes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Router, Route, browserHistory, IndexRedirect, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+// import axios from 'axios';
+
+import store from './store';
+
+import App from './components/App';
+// import  from './containers/';
+
+// import { receiveClothings } from './action-creators/categories';
+// import { receiveClothings } from './action-creators/clothings';
+
+const onAppEnter = () => {
+//   const categories = axios.get('/api/categories');
+//   const clothing = axios.get('/api/clothings');
+
+  console.log('OnAppEnter called !!!');
+//   return Promise
+//     .all([categories, clothing])
+//     .then(responses => responses.map(r => r.data))
+//     .then(([retCategories, retClothings]) => {
+//       store.dispatch(receiveCategories(retCategories));
+//       store.dispatch(receiveClothings(retClothings));
+//     });
+};
+
+
+export default function Root() {
+  return (
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={App} onEnter={onAppEnter} />
+      </Router>
+    </Provider>
+  );
+}
