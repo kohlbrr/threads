a792169fd4057fe700cf9bc236e9f049352f0ab4
kohlbrr <kohlbrr@gmail.com>
2017-06-08 15:53:25 -0400

    test(redux, 77): Working on building TDD tests for redux store

---
+++ server/specs/frontEnd/redux/reducers/root-reducer.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import { createStore } from 'redux';
+import mainReducer from '../../../../../browser/src/react/reducers/root-reducer';
+
+describe('Root reducer', () => {
+  
+  let testStore;
+  beforeEach('Create a test store', () => {
+    
+  });
+
+});
