e3609e2dac3502f8849260b56872679a14d3a7af
kohlbrr <kohlbrr@gmail.com>
2017-06-09 12:08:38 -0400

    test(redux, 77): Modified what the tests were looking for - need to make spies

--- server/specs/frontEnd/redux/reducers/root-reducer.spec.js
+++ server/specs/frontEnd/redux/reducers/root-reducer.spec.js
@@ -29,70 +29,102 @@ describe('Root reducer', () => {
 
   // currentUser
   describe('SET_CURRENT_USER', () => {
-    it('sets the current user', () => {
-      testStore.dispatch({ type: 'SET_CURRENT_USER', userId: userId }); // TODO
+    it('sets the current user from the db', () => {
+      testStore.dispatch({ type: 'SET_CURRENT_USER', userId: 1 });
       const newState = testStore.getState();
-      expect(newState.currentUser).to.be.deep.equal(/*user with ID in the db*/);
+      // Spy that the action fired the thunk func
+      //expect(newState.currentUser).to.be.deep.equal(); // Potentially not needed
     });
   });
 
   // cart
   describe('RECEIVE_CART', () => {
     it('populates a users cart from the db', () => {
-      testStore.dispatch({ type: 'RECEIVE_CART', userId: userId }); // CurrentUser ID or a var for admins
+      testStore.dispatch({ type: 'RECEIVE_CART', userId: 1 }); // CurrentUser ID or a var for admins
       const newState = testStore.getState();
-      // test if equal
+      // Spy that the action fired the thunk func
     });
   });
 
-  describe('ADD_CART_ITEM', () => {
-    it('adds a cartcontent to the store', () => {});
+  describe('ADD_CART_CONTENT', () => {
+    const cartContent = {
+      quantity: 2,
+      userId: 1,
+      productId: 2
+    };
+    it('adds a cartcontent to the store', () => {
+      testStore.dispatch({ type: 'ADD_CART_CONTENT', cartContent: cartContent });
+      const newState = testStore.getState();
+      expect(newState.cart[0]).to.be.deep.equal(cartContent);
+    });
   });
 
-  describe('REMOVE_CART_ITEM', () => {
-    it('removes a cartcontent from the store cart', () => {});
+  describe('REMOVE_CART_CONTENT', () => {
+    it('removes a cartcontent from the store cart', () => {
+      testStore.dispatch({ type: 'REMOVE_CART_CONTENT', cartContentId: 1 });
+      const newState = testStore.getState();
+      // Spy that the action fired the thunk func
+    });
   });
 
   // orders
   describe('RECEIVE_ORDERS', () => {
-    it('populates orders form the db', () => {});
+    it('populates orders form the db', () => {
+      const newState = testStore.getState();
+    });
   });
 
   describe('ADD_ORDER', () => {
-    it('adds an order to the store', () => {});
+    it('adds an order to the store', () => {
+      const newState = testStore.getState();
+    });
   });
 
   // currentOrder
   describe('SET_CURRENT_ORDER', () => {
-    it('sets the current order', () => {});
+    it('sets the current order', () => {
+      const newState = testStore.getState();
+    });
   });
   
   // currentProduct
   describe('SET_CURRENT_PRODUCT', () => {
-    it('sets the current product', () => {});
+    it('sets the current product', () => {
+      const newState = testStore.getState();
+    });
   });
   
   // currentDesign
   describe('SET_CURRENT_DESIGN', () => {
-    it('sets the current design', () => {});
+    it('sets the current design', () => {
+      const newState = testStore.getState();
+    });
   });
 
   // categories
   describe('RECEIVE_CATEGORIES', () => {
-    it('populates categories from the db', () => {});
+    it('populates categories from the db', () => {
+      const newState = testStore.getState();
+    });
   });
 
   describe('ADD_CATEGORY', () => {
-    it('adds a category to the store');
+    it('adds a category to the store', () => {
+      const newState = testStore.getState();
+    });
   });
 
   describe('REMOVE_CATEGORY', () => {
-    it('removes a category from the store');
+    it('removes a category from the store', () => {
+      const newState = testStore.getState();
+    });
   });
 
   // currentCategory
   describe('SET_CURRENT_CATEGORY', () => {
-    it('sets the current category', () => {});
+    it('sets the current category', () => {
+      const newState = testStore.getState();
+    });
   });
 
 });
