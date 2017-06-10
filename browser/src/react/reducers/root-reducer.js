9acdbab87b26a1e089a969c6932a23333c1e14e3
Hubert J <hubejan@gmail.com>
2017-06-10 17:15:04 -0400

    Current user and Users redux stuff

--- browser/src/react/reducers/root-reducer.js
+++ browser/src/react/reducers/root-reducer.js
@@ -3,10 +3,14 @@ import categoriesReducer from './categories-reducer';
 import designsReducer from './designs-reducer';
 import productReducer from './product-reducer';
 import currentDesignReducer from './currentDesign-reducer';
+import usersReducer from './users-reducer';
+import currentUser from './currentUser-reducer';
 
 export default combineReducers({
   categories: categoriesReducer,
   designs: designsReducer,
   currentProduct: productReducer,
   currentDesign: currentDesignReducer,
+  users: usersReducer,
+  currentUser,
 });
