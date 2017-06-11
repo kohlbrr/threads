4faf6e523fe882368d707d8ae2b18af1f5eabd53
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-10 20:41:26 -0400

    images loading in designView

--- browser/src/react/components/ProductPreview.js
+++ browser/src/react/components/ProductPreview.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+
+function getImage(imgUrl) {
+  const img = document.images[0];
+  const imageLoading = new Image();
+  if(img) {
+    img.src = 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif';
+  }
+  imageLoading.onload = function attachSourceToImage() {
+    img.src = this.src;
+    console.log('image loaded');
+  };
+  console.log('hello')
+  imageLoading.src = imgUrl;
+  return null;
+}
+
 const ProductPreview = ({ design, imageUrl }) => (
   <div>
     <h1>{design.name}</h1>
-    <img className="img-thumbnail" alt="Product" src={imageUrl || design.imageUrl} />
+    <img className="img-thumbnail" alt="Product" src={''} />
+    {getImage(imageUrl || design.imageUrl)}
   </div>
 );
 
