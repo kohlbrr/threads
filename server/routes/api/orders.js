23e462954d7d77f074fe4a7cbefbe62c726fc2b1
kohlbrr <kohlbrr@gmail.com>
2017-06-12 14:14:02 -0400

    enhancement(orders api, 73): Added checks to routes and updated create to work off of cartContents

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = new express.Router();
+const { isLoggedIn, isAdmin } = require('../../middleware');
 const { Order, OrderProducts } = require('../../db/models');
 module.exports = router;
 
 // Get al orders
 // ADMIN
-router.get('/', (req, res, next) => {
+router.get('/', isAdmin, (req, res, next) => {
   Order.findAll()
   .then(orders => res.json(orders))
   .catch(next);
@@ -13,15 +14,31 @@ router.get('/', (req, res, next) => {
 
 // Create an order
 // USER
-router.post('/', (req, res, next) => {
-  Order.create(req.body)
-  .then(order => res.status(201).send(order))
+router.post('/', isLoggedIn, (req, res, next) => {
+  // Create order
+  Order.create({
+    status: 'Pending',
+    timestamp: Date.now(),
+    userId: req.user && req.user.id
+  })
+  // Create order items associated with order
+  .then(order => {
+    req.body.forEach(cartContent => {
+      OrderProducts.create({
+        orderId: order.id,
+        productId: cartContent.productId,
+        price: cartContent.price,
+        quantity: cartContent.quantity
+      });
+    });
+  })
+  .then(() => res.sendStatus(201))
   .catch(next);
 });
 
 // Update an order status
 // ADMIN
-router.put('/:id', (req, res, next) => {
+router.put('/:id', isAdmin, (req, res, next) => {
   Order.update(
     req.body,
     {
@@ -37,18 +54,24 @@ router.put('/:id', (req, res, next) => {
 
 // Return a single order
 // USER (gated)
-router.get('/:id', (req, res, next) => {
-  Order.findById(req.params.id)
-  .then(order => res.send(order))
-  .catch(next);
+router.get('/:id', isLoggedIn, (req, res, next) => {
+  if(req.user.isAdmin || req.user.id === req.params.id) {
+    Order.findById(req.params.id)
+    .then(order => res.send(order))
+    .catch(next);
+  } else { res.sendStatus(403); }
 });
 
-router.get('/:id/items', (req, res, next) => {
-  OrderProducts.findAll({
-    where: { orderId: req.params.id },
-  })
-  .then(orderProducts => res.send(orderProducts))
-  .catch(next);
+// Return all order items
+// USER (gated)
+router.get('/:id/items', isLoggedIn, (req, res, next) => {
+  if(req.user.isAdmin || req.user.id === req.params.id) {
+    OrderProducts.findAll({
+      where: { orderId: req.params.id },
+    })
+    .then(orderProducts => res.send(orderProducts))
+    .catch(next);
+  } else { res.sendStatus(403); }
 });
 
 // We don't want to delete orders via API routes - can't forsee a reason to
