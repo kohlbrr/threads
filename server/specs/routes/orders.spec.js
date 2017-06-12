11084fe057b72bcf49a7860a4ddfe29129326bc7
kohlbrr <kohlbrr@gmail.com>
2017-06-12 15:37:15 -0400

    test(orders api, 113): Added beginning of orders tests

---
+++ server/specs/routes/orders.spec.js
@@ -0,0 +1,74 @@
+const { expect } = require('chai');
+const session = require('supertest-session');
+const app = require('../../app');
+const { Product, User, Order, OrderProducts } = require('../../db/models');
+
+const agent = session(app);
+
+describe('Orders API', () => {
+  
+  // Products
+  // Users
+  // To have Orders
+  // That have OrderProducts
+
+  beforeEach(() => Product.sync({ force: true }))
+  .then(() => User.sync({ force: true }))
+  .then(() => Order.sync({ force: true }))
+  .then(() => OrderProducts.sync({ force: true }));
+  
+  beforeEach(() => Product.create({
+    size: 'S',
+    color: 'Blue',
+    designId: 1,
+    stock: 30
+  }))
+  .then(() => User.create({
+    email: 'thunk@krunk.dunk',
+    password: '123456',
+    isAdmin: true
+  }))
+  .then(() => User.create({
+    email: 'normie@bland.toast',
+    password: 'iWishIWasAnAdmin',
+    isAdmin: false
+  }))
+  .then(user => Order.create({
+    status: 'Pending',
+    timestamp: 0,
+    userId: user.id
+  }))
+  .then(order => OrderProducts.create({
+    orderId: order.id,
+    productId: 1,
+    price: 19.95,
+    quantity: 2
+  }));
+
+  describe('GET /api/orders', () => {
+    it('should 403 if there is no user or user is not an admin', () => {
+      agent.get('/api/orders').expect(403);
+    });
+    describe('Admin', () => {
+      let loggedInUser;
+      beforeEach(() => {
+        loggedInUser = session(app);
+        return loggedInUser.post('/auth/login')
+        .send({ email: 'think@krunk.dunk', password: '123456' });
+      });
+      it('should return a 200', () => loggedInUser.get('/api/orders').expect(200));
+      it('should return an array of orders', () => {
+        loggedInUser.get('/api/orders').expect(res => {
+          expect(res.body).to.be.an('array');
+          expect(res.body[0].color).to.equal('Blue');
+        });
+      });
+    });
+  });
+
+  // Create an order || USER
+  // Update order status || ADMIN
+  // Return one order || USER
+  // Return all order items || USER
+
+});
