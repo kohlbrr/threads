058fd9b1aadebb0f4be853cc2fe9f0d9cbee43c5
Joon Kim <jkim7078@gmail.com>
2017-06-14 03:49:36 -0400

    merge conflicts

--- browser/src/react/action-creators/reviews.js
+++ browser/src/react/action-creators/reviews.js
@@ -1,33 +1,28 @@
 import { RECEIVE_REVIEWS, ADD_REVIEW } from '../constants';
 import axios from 'axios';
-import { ADD_REVIEW } from '../constants'
 import store from '../store';
 
 export const receiveReviews = reviews => ({
   type: RECEIVE_REVIEWS,
-  reviews
+  reviews,
 });
 
-export const addReview = review => ({
+export const addReviewAction = review => ({
   type: ADD_REVIEW,
-  review
+  review,
 });
 
 export const getReviews = () => (
   axios.get('/api/reviews')
   .then(res => res.data)
-  .then(reviews => dispatch(receiveReviews(reviews)))
+  .then(reviews => store.dispatch(receiveReviews(reviews)))
   .catch(console.error));
 
 export const newReview = review => (
   axios.post('/api/reviews', review)
   .then(res => res.data)
-  .then(addedReview => dispatch(addReview(addedReview)))
-  
-export const addReviewAction = review => ({
-  type: ADD_REVIEW,
-  review,
-});
+  .then(addedReview => store.dispatch(addReviewAction(addedReview))))
+
 
 export const addReview = (stars, content, designId) => (
   axios.post('/api/reviews', { stars, content, designId })
